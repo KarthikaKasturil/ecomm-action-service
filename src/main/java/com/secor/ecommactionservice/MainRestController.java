93b28efecea6d63ae43ca8787481de0ce7b9520b
Karthika Kasturil <karthika.kasthuril@gmail.com>
2025-07-14 12:37:35 +0530

    update

--- src/main/java/com/secor/ecommactionservice/MainRestController.java
+++ src/main/java/com/secor/ecommactionservice/MainRestController.java
@@ -60,9 +60,12 @@ public class MainRestController {
                 payment.setCustomerId(savedOrder.getCustomerId());
                 payment.setPaymentMethod("ONLINE");
                 payment.setAmount(totalAmount);
-                ResponseEntity<?> result = actionService.createPayment(payment);
-                LOG.info("Payment created: {}", result);
-                return ResponseEntity.ok("Order initiated successfully and created payment");
+                payment.setStatus("PENDING");
+                payment.setPaymentDate(LocalDateTime.now());
+                payment.setUpdatedAt(LocalDateTime.now());
+                Payment savedPayment = actionService.savePayment(payment);
+                LOG.info("Payment created: {}", savedPayment);
+                return ResponseEntity.ok("Order initiated successfully with order Id: " + savedOrder.getOrderId() + " and created payment with paymentId: " + savedPayment.getPaymentId());
             }
         }
     }
@@ -73,58 +76,62 @@ public class MainRestController {
         LOG.info("processPayment({})", status);
         Payment payment = actionService.getPaymentByOrder(status.getOrderId());
         if (payment != null) {
-            payment.setStatus(status.getStatus());
-            payment.setUpdatedAt(LocalDateTime.now());
-            Payment updatedPayment = actionService.savePayment(payment);
-            LOG.info("Payment status updated to " + updatedPayment.getStatus() + " for order " + updatedPayment.getOrderId());
-            if("COMPLETED".equalsIgnoreCase(updatedPayment.getStatus())) {
-                LOG.info("Check inventory for order id: " + updatedPayment.getOrderId());
-                InventoryItem inventoryItem = actionService.checkInventory(updatedPayment.getOrderId());
-                Order orderdetails = actionService.getOrderDetails(updatedPayment.getOrderId());
-                if (inventoryItem != null && inventoryItem.getQuantity() >= orderdetails.getQuantity() ) {
-                    LOG.info("Update inventory for order id: " + updatedPayment.getOrderId());
-                    inventoryItem.setQuantity(inventoryItem.getQuantity() - orderdetails.getQuantity());
-                    inventoryItem.setLastUpdated(LocalDateTime.now());
-                    InventoryItem updatedInventory = actionService.updateInventory(inventoryItem);
-                    LOG.info("Inventory updated: " + updatedInventory);
+            if("PENDING".equalsIgnoreCase(payment.getStatus())){
+                payment.setStatus(status.getStatus());
+                payment.setUpdatedAt(LocalDateTime.now());
+                Payment updatedPayment = actionService.savePayment(payment);
+                LOG.info("Payment status updated to " + updatedPayment.getStatus() + " for order " + updatedPayment.getOrderId());
+                if ("COMPLETED".equalsIgnoreCase(updatedPayment.getStatus())) {
+                    LOG.info("Check inventory for order id: " + updatedPayment.getOrderId());
+                    Order orderDetails = actionService.getOrderDetails(updatedPayment.getOrderId());
+                    InventoryItem inventoryItem = actionService.checkInventory(orderDetails.getProductId());
 
-                    LOG.info("Update order status to confirmed for " + updatedPayment.getOrderId());
-                    OrderStatus orderStatus = new OrderStatus();
-                    orderStatus.setOrderId(updatedPayment.getOrderId());
-                    orderStatus.setStatus("CONFIRMED");
-                    actionService.updateOrderStatus(orderStatus);
-                    LOG.info("Order status updated as confirmed for " + orderdetails.getOrderId());
+                    if (inventoryItem != null && inventoryItem.getQuantity() >= orderDetails.getQuantity()) {
+                        LOG.info("Update inventory for order id: " + updatedPayment.getOrderId());
+                        inventoryItem.setQuantity(inventoryItem.getQuantity() - orderDetails.getQuantity());
+                        inventoryItem.setLastUpdated(LocalDateTime.now());
+                        InventoryItem updatedInventory = actionService.updateInventory(inventoryItem);
+                        LOG.info("Inventory updated: " + updatedInventory);
 
-                    LOG.info("Add order for customer " + updatedPayment.getCustomerId());
-                    OrderInfo orderInfo = new OrderInfo();
-                    orderInfo.setOrderId(updatedPayment.getOrderId());
-                    orderInfo.setCustomerId(updatedPayment.getCustomerId());
-                    actionService.updateCustomerOrder(orderInfo);
-                    LOG.info("Order added for customer " + updatedPayment.getCustomerId());
-                    return ResponseEntity.ok("Payment processed successfully and order confirmed for " + updatedPayment.getOrderId());
-                } else {
-                    LOG.info("Insufficient inventory for order id: " + updatedPayment.getOrderId());
-                    LOG.info("Update order status to cancelled for " + updatedPayment.getOrderId());
+                        LOG.info("Update order status to confirmed for " + updatedPayment.getOrderId());
+                        OrderStatus orderStatus = new OrderStatus();
+                        orderStatus.setOrderId(updatedPayment.getOrderId());
+                        orderStatus.setStatus("CONFIRMED");
+                        actionService.updateOrderStatus(orderStatus);
+                        LOG.info("Order status updated as confirmed for " + orderDetails.getOrderId());
+
+                        LOG.info("Add order for customer " + updatedPayment.getCustomerId());
+                        OrderInfo orderInfo = new OrderInfo();
+                        orderInfo.setOrderId(updatedPayment.getOrderId());
+                        orderInfo.setCustomerId(updatedPayment.getCustomerId());
+                        ResponseEntity<?> result = actionService.updateCustomerOrder(orderInfo);
+                        LOG.info("Order creation: " + result.toString());
+                        return ResponseEntity.ok("Payment processed successfully and order confirmed for " + updatedPayment.getOrderId());
+                    } else {
+                        LOG.info("Insufficient inventory for order id: " + updatedPayment.getOrderId());
+                        LOG.info("Update order status to cancelled for " + updatedPayment.getOrderId());
+                        OrderStatus orderStatus = new OrderStatus();
+                        orderStatus.setOrderId(updatedPayment.getOrderId());
+                        orderStatus.setStatus("CANCELLED");
+                        ResponseEntity<?> orderResponse = actionService.updateOrderStatus(orderStatus);
+                        LOG.info("Order status updated as cancelled for " + orderDetails.getOrderId());
+
+                        LOG.info("Update payment status as refunded for " + updatedPayment.getOrderId());
+                        updatedPayment.setStatus("REFUND_INITIATED");
+                        updatedPayment.setUpdatedAt(LocalDateTime.now());
+                        actionService.savePayment(updatedPayment);
+                        return ResponseEntity.status(400).body("Insufficient inventory for order " + updatedPayment.getOrderId() + ". Order cancelled and payment status updated to REFUND_INITIATED.");
+                    }
+                } else if ("FAILED".equalsIgnoreCase(updatedPayment.getStatus())) {
+                    LOG.info("update order status for " + updatedPayment.getOrderId());
                     OrderStatus orderStatus = new OrderStatus();
                     orderStatus.setOrderId(updatedPayment.getOrderId());
                     orderStatus.setStatus("CANCELLED");
-                    ResponseEntity<?> orderResponse = actionService.updateOrderStatus(orderStatus);
-                    LOG.info("Order status updated as cancelled for " + orderdetails.getOrderId());
-
-                    LOG.info("Update payment status as refunded for " + updatedPayment.getOrderId());
-                    updatedPayment.setStatus("REFUND_INITIATED");
-                    updatedPayment.setUpdatedAt(LocalDateTime.now());
-                    actionService.savePayment(updatedPayment);
-                    return ResponseEntity.status(400).body("Insufficient inventory for order " + updatedPayment.getOrderId() + ". Order cancelled and payment status updated to REFUND_INITIATED.");
+                    actionService.updateOrderStatus(orderStatus);
+                    return ResponseEntity.ok("Payment failed for order " + updatedPayment.getOrderId() + ". Order status updated to CANCELLED.");
                 }
-            }
-            else if("FAILED".equalsIgnoreCase(updatedPayment.getStatus())) {
-                LOG.info("update order status for " + updatedPayment.getOrderId());
-                OrderStatus orderStatus = new OrderStatus();
-                orderStatus.setOrderId(updatedPayment.getOrderId());
-                orderStatus.setStatus("CANCELLED");
-                actionService.updateOrderStatus(orderStatus);
-                return ResponseEntity.ok("Payment failed for order " + updatedPayment.getOrderId() + ". Order status updated to CANCELLED.");
+            } else {
+               return ResponseEntity.ok("Order is already processed with paymentId: "+ payment.getPaymentId());
             }
         }
         return ResponseEntity.status(400).body("Payment not found for order " + status.getOrderId());
